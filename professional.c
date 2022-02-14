8e35cc3190834b74cb220f1d6fd67e5cd4886084
Aliya's folder <aliya.bermaganbet@alumni.nu.edu.kz>
2022-02-14 14:32:52 +0300

    Testing the commits i did

--- professional.c
+++ professional.c
@@ -2,5 +2,6 @@
 int main()
 {
  printf("I am creating my final project!\n");
+ printf("I am adding some commits to the final projectin order to test whether my commits are added in the right way!\n");
  return 0;
 }
